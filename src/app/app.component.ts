91b6baaca2f7bd9057ac8e341df32c46eede5bcf
DNGriffin <31415269+DNGriffin@users.noreply.github.com>
2020-08-09 22:35:07 -0500

    changed resume content, updated game content, fixed game text hitbox bug, enhanced umbrella movement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -46,12 +46,19 @@ export class AppComponent extends Phaser.Scene implements OnInit {
   }
   title = 'Devin Griffin';
   preload() {
+    if(innerWidth<800){
+      this.isPhone = true;
+    }
     this.cameras.main.setBackgroundColor("#F6F5ED");
-    this.load.image("umbrella", "assets/game/umbrella.png");
-    this.load.image("droplet", "assets/game/droplet.png");
-
-
+    if(this.isPhone){
+      this.load.image("umbrella", "assets/game/umbrellasmall.png");
+      this.load.image("droplet", "assets/game/dropletsmall.png");
+    }else{
+      this.load.image("umbrella", "assets/game/umbrella.png");
+      this.load.image("droplet", "assets/game/droplet.png");
+    }
   }
+  isPhone = false;
   gameText;
   umbrella: Phaser.GameObjects.Sprite;
   score = 0;
@@ -92,7 +99,6 @@ export class AppComponent extends Phaser.Scene implements OnInit {
     this.physics.add.overlap(droplet, this.umbrella, this.umbrellaCollide, null, this);
     this.physics.add.overlap(droplet, this.sentenceSprites, this.textCollide, null, this);
     this.droplets[(this.dropletIndex++) % 5] = droplet;
-
   }
   getFurtherestRightSentence() {
     var maxX = this.sentenceSprites[0].x;
@@ -114,8 +120,13 @@ export class AppComponent extends Phaser.Scene implements OnInit {
   }
   gamePaused = true;
   endGame() {
+    this.canResumeGame = false;
+    setTimeout(()=>{this.canResumeGame = true;},1000);
     this.resetScore();
-    alert("You let the text get rained on, score reset. Click the umbrella to resume the game. ")
+
+    this.gameText.setVisible(true);
+    this.gameText.setText("You let the text get rained on, score reset.\nClick to resume the game. ")
+    
     this.gamePaused = true;
     this.sentenceSprites.forEach(sentence => {
       sentence.body.velocity.x = 0
@@ -128,12 +139,12 @@ export class AppComponent extends Phaser.Scene implements OnInit {
   textCollide(droplet, text) {
     droplet.destroy();
     this.endGame();
-    // this.restartGame();
   }
   resumeGame() {
-    if (!this.gamePaused) {
+    if (!this.gamePaused || !this.canResumeGame) {
       return;
     }
+    setTimeout(() => { this.spawnUpsideDown = true }, 10000);
     this.gameText.setVisible(false);
     this.gamePaused = false;
     this.sentenceSprites.forEach(sentence => {
@@ -147,31 +158,20 @@ export class AppComponent extends Phaser.Scene implements OnInit {
   }
   spawnUpsideDown = false;
   create() {
-    setTimeout(() => { this.spawnDroplet(); }, 500);
-    setTimeout(() => { this.spawnDroplet(); }, 100);
-    setTimeout(() => { this.spawnDroplet(); }, 1500);
-    setTimeout(() => { this.spawnDroplet(); }, 2000);
-    setTimeout(() => { this.spawnUpsideDown = true }, 3000);
     this.createText();
-    this.umbrella = this.physics.add.sprite(innerWidth / 2, this.gameHeight * 0.2, "umbrella")
+    this.umbrella = this.physics.add.sprite(innerWidth / 2, this.gameHeight * 0.75, "umbrella")
     this.umbrella.setInteractive();
-    this.umbrella.displayWidth = innerWidth / 10;
+    if(this.isPhone){
+      this.umbrella.displayWidth = innerWidth / 5;
+    }else{
+      this.umbrella.displayWidth = innerWidth / 10;
+    }
     this.umbrella.scaleY = this.umbrella.scaleX;
-    this.input.setDraggable(this.umbrella);
-    this.input.on('drag', (pointer, gameObject, dragX, dragY) => {
-
-      gameObject.x = dragX;
-      gameObject.y = dragY;
-    });
     this.input.on('pointerdown', (pointer, gameObject, dragX, dragY) => {
       console.log("pointer down");
       this.resumeGame();
     });
-
     this.spawnDroplet();
-
-
-
   }
   restartGame() {
     this.sentenceSprites.forEach(sentence => {
@@ -183,13 +183,17 @@ export class AppComponent extends Phaser.Scene implements OnInit {
   sentenceIndex = 0;
   sentenceSprites = [];
   createText() {
-    this.gameText = this.add.text(innerWidth / 2, this.gameHeight / 2, "Click on the umbrella to start the game.\nDrag the umbrella to keep the text dry.").setOrigin(0.5, 0.5).setFontSize(innerWidth * 0.035).setFontFamily('Verdana, "Times New Roman", Tahoma, serif').setFill("black");
+    this.gameText = this.add.text(innerWidth / 2, this.gameHeight *0.25, "Click on the umbrella to start the game.\nDrag the umbrella to keep the text dry.").setOrigin(0.5, 0.5).setFontFamily('Verdana, "Times New Roman", Tahoma, serif').setFill("black");
+    var gameTextSize = innerWidth * 0.035;
+    if(gameTextSize>40){
+      gameTextSize = 40;
+    }
+    this.gameText.setFontSize(gameTextSize);
 
     for (var i = 0; i < 3; i++) {
       var sentenceSprite;
-      sentenceSprite = this.add.text(innerWidth / 2, this.gameHeight / 2, this.sentences[this.sentenceIndex++]).setOrigin(0, 0.5).setFontSize(innerHeight * 0.1).setFontFamily('Verdana, "Times New Roman", Tahoma, serif').setFill("black");
+      sentenceSprite = this.add.text(innerWidth / 2, this.gameHeight / 2, this.sentences[this.sentenceIndex++]).setOrigin(0, 0.5).setFontSize(innerHeight * 0.06).setFontFamily('Verdana, "Times New Roman", Tahoma, serif').setFill("black");
       this.physics.world.enable(sentenceSprite);
-      // sentenceSprite.body.velocity.x = -innerWidth / 4;
       this.sentenceSprites.push(sentenceSprite);
     }
     this.sentenceSprites[0].x = innerWidth;
@@ -197,6 +201,7 @@ export class AppComponent extends Phaser.Scene implements OnInit {
     this.sentenceSprites[2].x = this.sentenceSprites[1].x + this.sentenceSprites[1].displayWidth + innerWidth / 6;
 
   }
+  canResumeGame = true;
   update() {
     if (this.umbrella.y > this.gameHeight / 2) {
       this.umbrella.flipY = true;
@@ -210,39 +215,49 @@ export class AppComponent extends Phaser.Scene implements OnInit {
         this.sentenceIndex %= this.sentences.length;
 
         sentence.setText(this.sentences[this.sentenceIndex])
+        sentence.body.setSize(sentence.displayWidth, sentence.displayHeight);
         sentence.x = this.getFurtherestRightSentence().x + this.getFurtherestRightSentence().displayWidth + innerWidth / 6;
       }
     });
+
+    var pointer = this.input.activePointer;
+    if (pointer.isDown) {
+        if(pointer.y >= 0 && pointer.y <= this.gameHeight){
+          this.umbrella.x = pointer.x;
+          this.umbrella.y = pointer.y;
+        }
+    }
   }
   sentences = [
+    "Resume Content (Shortened for game)",
+    "Washington University Fall 2017 - May 2021",
     "B.S. in Computer Science. GPA: 3.64/4.0",
-    "Don Lang Scholar – full ride scholarship",
-    "Hagan Scholar – I manage a $15,000 Schwab investment account",
+    "Don Lang Scholar – full ride scholarship.",
+    "Hagan Scholar – I manage a ~$17,000 Schwab investment account.",
+    "Work Experience",
+    "Software Engineer Intern at AT&T (Jun 2020 - Aug 2020)",
     "Game Developer at Clear for Ears (Jun 2018 - Present)",
-    "Redesigned the entire suite of games to unify the graphic style and make them fun.",
-    "Used Phaser to create two new auditory rehabilitation games.",
-    "Used Angular and Firebase to redesign our website (previously AWS + PHP).",
-    "Interned Summer ’18 and stayed on since",
     "Web Developer at Washington University G.I.S. (Oct 2017 - May 2018)",
-    "Used Ruby on Rails, GeoBlacklight, PostgreSQL, and the Boundless Suite to create a geospatial discovery web app to house WashU’s spatial data",
     "Project Manager at DevSTAC (Sept 2017 - May 2018)",
-    "Lead a team that used formidable forms and the Service Now API to digitize incident report forms for Barnes Jewish Hospital",
-    "Teacher's Assistant at Washington University (Jan 2018 - Present)",
-    "Web Development for 3 semesters (Spring ‘18, Fall ‘18, Spring ‘19).",
-    "Introduction to Computer Security (Fall ’19)",
-    "       Projects        ",
-    "Elemental Fighter - Multiplayer 2D pixel art game that features lag free networking",
-    "Gamified Math Learning Platform - Online math learning platform that lets students compete against each other in math games.",
-    "Sello (Letgo for Universities) - iOS buying and selling platform for college students",
-    "Family Fun Center App - App that keeps track of schedule and hours worked by employees of a family fun center.",
-    "        Awards        ",
+    "Teacher's Assistant at Washington University (Jan 2018 - Jan 2020)",
+    "Coin Toss (2020) - iOS App and Web App that uses simulation games to make unimportant decisions for you (ex: which coffee should I get today). Angular, Firebase, Phaser",
+    "Published in Apple App Store (CoinTossDecisions)",
+    "Pong Command Line Edition (2020) - Created a two player, single player, and single player vs ai pong game. LibGDX",
+    "Multiplayer Math (2019) - High School & College course supplement tool where students respond to instructor questions by playing networked cooperative games. Angular + Firebase",
+    "Elemental Fighter (2019) - Multiplayer 2D pixel art game that features lag free networking and skill-based combat. Unity2D",
+    "Math Battles (2018) - Online math learning platform that lets students compete against each other in math games, to provide an incentive to learn math. React, Phaser, MySQL, SocketIO",
+    "Sello (Letgo for Universities) (2018) - iOS buying and selling platform for college students which allows you to filter by University. Swift, Firebase",
+    "Family Fun Center App (2017) - App that keeps track of schedule and hours worked by employees of a family fun center. Python, SQLite, Kivy",
+    "Awards",
     "3rd in Programming and Coding at Missouri FBLA State Convention",
     "4th in Computer Programming at Missouri State Pummel Relays",
     "9th in Computer Problem Solving at Missouri FBLA State Convention",
     "Won Y’s Thoughts Entrepreneurship challenge with a Table Tennis Matchmaking app.",
-    "Proficient: Java, JavaScript, TypeScript, Phaser",
-    "Basic: Python, C++, C#, Arduino C, PHP, Swift, React, Angular, Unity, Firebase, AWS, jQuery, MySQL",
-    "Classes (1/2) Data Structures and Algorithms, Computer Science II (Arduino Class), Web Development, Object-Oriented Software Development Laboratory (C++)",
-    "Classes (2/2) Rapid Prototype Development and Creative Programming (Full Stack Web Dev), Mobile Application Development (iOS Dev in Swift), Introduction to Computer Security, Video Game Programming (Unity)",
+    "Proficient: Java, JavaScript, TypeScript, Phaser, Angular, Firebase",
+    "Basic: Python, C++, C#, Arduino C, PHP, Swift, React, Unity, AWS, jQuery, MySQL",
+    "Relevant Classes: Data Structures and Algorithms, Computer Science II (Arduino Class)",
+    "Web Development, Object-Oriented Software Development Laboratory (C++), Rapid Prototype Development and Creative Programming (Full Stack Web Dev)",
+    "Mobile Application Development (iOS Dev in Swift), Introduction to Computer Security, Video Game Programming (Unity), Analysis of Algorithms",
+    "Mobile App Development II, Programming Tools and Techniques, Video Game Programming II, Internet of Things, Software Engineering Workshop, Introduction to Systems Software",
   ]
 }
